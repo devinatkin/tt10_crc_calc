84e322f4a5fd1f4be90454765130a1c3aa071cfd
Devin <devinatkin@gmail.com>
2024-11-13 22:05:52 -0700

    remove non existent values

--- test/test.py
+++ test/test.py
@@ -16,9 +16,7 @@ async def test_project(dut):
 
     # Reset
     dut._log.info("Reset")
-    dut.ena.value = 1
     dut.ui_in.value = 0
-    dut.uio_in.value = 0
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
@@ -27,7 +25,6 @@ async def test_project(dut):
 
     # Set the input values you want to test
     dut.ui_in.value = 20
-    dut.uio_in.value = 30
 
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
