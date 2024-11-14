ed9e53e867127e377e16d80a8f07661a237a8eea
Devin <devinatkin@gmail.com>
2024-11-13 21:56:22 -0700

    fix test

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_devinatkin_crc user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
@@ -38,10 +38,6 @@ module tb ();
 
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
   );
