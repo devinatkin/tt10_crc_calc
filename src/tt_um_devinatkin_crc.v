a353184041dfab5594c1f981ba839986ae32e964
Devin <devinatkin@gmail.com>
2024-11-18 22:07:17 -0700

    Remove Old Code

--- src/tt_um_devinatkin_crc.v
+++ src/tt_um_devinatkin_crc.v
@@ -23,10 +23,4 @@ module tt_um_devinatkin_crc (
         .crc_out(uo_out)
     );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + ui_in;  // Example: ou_out is the sum of ui_in and uio_in
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{clk, rst_n, 1'b0};
-
 endmodule
